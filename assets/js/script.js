b956b1fa485329a9f6379292ae39d8ea7d8d5ec4
Github1234gfh <83724836+Github1234gfh@users.noreply.github.com>
2022-11-16 21:32:39 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,6 @@ if (document.querySelector('.field')) {
     document.querySelector('.field').onclick = function() {
         var ball = document.querySelector('.ball')
         var cord = [this.clientWidth, this.clientHeight, this.offsetTop, this.offsetLeft]
-        console.log(cord)
-        console.log(event.clientY-cord[2]-30, event.clientX-cord[3]-30)
-        console.log((cord[0] > 0 && cord[0] < 110 && cord[1] > 0 && cord[1] < 160))
         if ((event.clientY-cord[2]-30 > 0 && event.clientY-cord[2]-30 < 110) && (event.clientX-cord[3]-30 > 0 && event.clientX-cord[3]-30 < 160)) {
             ball.style.cssText = 
                 `top: ${event.clientY-cord[2]-30}px;`+
@@ -15,6 +12,7 @@ if (document.querySelector('.field')) {
         }
     };
 };
+
 // 2
 function ButtonClose() {
     var elem = document.querySelectorAll('.pane')
@@ -23,7 +21,15 @@ function ButtonClose() {
         button.setAttribute('class', 'remove-button')
         button.innerText = '[x]'
         elem[i].appendChild(button)
-        elem[i].getElementsByTagName('button')
     };
 };
 ButtonClose()
+
+// 3
+var elems = document.querySelectorAll('.remove-button')
+for (let i = 0; i < elems.length; i++) {
+    elems[i].onclick = function() {
+        this.parentElement.style.cssText = 
+        'display: none;';
+    }
+}
